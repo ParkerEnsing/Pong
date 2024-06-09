d840e48db0e81e81c5a7753d3e959f15512100f8
ParkerEnsing <parker.a.ensing@gmail.com>
2024-06-08 22:32:27 -0400

    Adds player paddle movement and a collision mesh

--- pong/assets/scripts/paddle.gd
+++ pong/assets/scripts/paddle.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	position[0] = 40;
+	position[1] = 0.5 * get_viewport_rect().size[1];
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position[1] = get_global_mouse_position()[1];
